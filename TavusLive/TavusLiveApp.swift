fd552ac209442cead74d198dfff81bed5f675b95
KowalewskiPawel <pawelkowalewsk@gmail.com>
2025-06-13 13:47:19 +0200

    Initial Commit

---
+++ TavusLive/TavusLiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  TavusLiveApp.swift
+//  TavusLive
+//
+//  Created by Pawel Kowalewski on 13/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct TavusLiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
